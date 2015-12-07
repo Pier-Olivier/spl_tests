b8f17a31c400be8ae414a88fbfb30b111b47b75b
pogregoire <pieroliviergregoire@gmail.com>
2015-12-07 15:39:12 +0100

    rewind iteratorIterator

--- iterator_iterator.php
+++ iterator_iterator.php
@@ -1,27 +1,35 @@
 <?php
 $pdo  = new PDO('mysql:host=localhost; dbname=safe_gestion', 'root', '');
 $stmt = $pdo->query("SELECT id_article,code_art,nom_art FROM article WHERE id_article<10");
-
+/*
 while( $rslt = $stmt->fetch()){
     var_dump($rslt);
 }
+*/
+
 echo '<hr />';
 $it = new IteratorIterator($stmt);
+var_dump($it->getInnerIterator());
+
+echo '<hr />';
+
+$it->rewind();
+while ($valeur = $it->current()){
+    echo $valeur[0].'='.$valeur[1].'<br />';
+    $it->next();
+}
+echo '<hr />';
+
+//$it->rewind();
+$it->next();
+var_dump($it->current()[1]);
 
 /*
-while ($valeur = $it->current()){
-    echo $valeur[0].'<br />';
-    $it->next();
- $it->rewind();
-}
-*/
-$it->rewind();
-$it->next();
-var_dump($it->current());
 echo '<hr />';
 foreach ($it as $val) {
-//    echo $val[0].'<hr />';
-    var_dump($val);
+    echo $val[0].'<br />';
+//    var_dump($val);
 }
+*/
 echo '<hr />';
-
+ 
