d624b6c55961d9a693d932653af8a9078b36efa1
pogregoire <pieroliviergregoire@gmail.com>
2015-12-08 11:56:00 +0100

    iterator_aggregate + countable

--- iterator_aggregate.php
+++ iterator_aggregate.php
@@ -8,13 +8,18 @@ class Personne {
 
     protected $_nom;
     protected $_prenom;
+    protected $_Description;
 
-    public function __construct($nom,$prenom) {
+    public function __construct($nom,$prenom, Description $Description) {
         $this->set_nom($nom);
         $this->set_prenom($prenom);
-
+        $this->set_Description($Description);
     }
 
+    public function set_Description(Description $Description)  {
+        $this->_Description = $Description;
+    }
+    
     public function set_nom($nom)  {
         $this->_nom = $nom;
     }
@@ -30,42 +35,106 @@ class Personne {
     public function prenom(){
         return $this->_prenom;
     }
+    
+    public function description_age() {
+        return $this->_Description->age();
+    }
+}
+
+class Description {
+    
+    protected $_age;
+    protected $_poids;
+
+    public function __construct($age, $poids) {
+        $this->set_age($age);
+        $this->set_poids($poids);
+    }
+    
+    public function set_age($age) {
+        $this->_age = $age;
+    }
+
+    public function age() {
+        return $this->_age;
+    }
+    
+    public function set_poids($poids) {
+        $this->_poids = $poids;
+    }
 }
 
 class Vehicule {
-    public static $_n;
+    public static $_n = 0;
     public function __construct() {
-        self::$_n = 0;
         self::$_n++;
-        echo '<h2>'.self::$_n.'</h2>';
     }
-    
+
+    public function nbr2vehicule() {
+        return self::$_n;
+    }
 }
 
-class Voiture extends Vehicule implements IteratorAggregate {
+class Voiture extends Vehicule implements IteratorAggregate, Countable {
     protected $_personnes = array();
     
     public function ajouter(Personne $p)  {
         $this->_personnes[] = $p;
+        return $this;
     }
     
-    public function getIterator()
-    {
-        return new ArrayIterator($this->_personnes);
+    public function getIterator(){
+        $reference = &$this->_personnes;
+        return new ArrayIterator($reference);
+    }
+ 
+    public function count() {
+        $n = 0;
+        while ($Objet = current($this->_personnes)){
+            if ($Objet->description_age()>=18)
+                $n++;
+            next($this->_personnes);
+        }
+        return $n;
+
+    }
+    public function & personne() {
+        $reference = &$this->_personnes;
+        return $reference;
     }
 }
 
-$P1 = new Personne('A','aa');
-$P2 = new Personne('B','bb');
+$D1= new Description(18, 70);
+$D2= new Description(6, 25);
+
+$P1 = new Personne('A','aa',$D1);
+$P2 = new Personne('B','bb',$D2);
 
 $Voiture = new Voiture();
-$Voiture->ajouter($P1);
-$Voiture->ajouter($P2);
+$Voiture->ajouter($P1)
+        ->ajouter($P2);
 
+//echo $Voiture->nbr2vehicule();
 //var_dump($Voiture->getIterator());
 
-foreach ($Voiture->getIterator() as $Objet){
-    echo $Objet->nom().'<br />';
+$ItAg = $Voiture->getIterator();
+var_dump($ItAg->current()->nom());
+$ItAg->next();
+var_dump($ItAg->current()->nom());
+$ItAg->next();
+var_dump($ItAg->current());
+
+var_dump($ItAg->count());
+
+var_dump($Voiture->count());
+
+
+foreach ($ItAg as $Personne){
+    echo $Personne->nom().''.$Personne->description_age().'<br />';
 }
 
- 
+$listePersonne = $Voiture->personne();
+var_dump(current($listePersonne)->nom());
+next($listePersonne);
+var_dump(current($listePersonne)->nom());
+
