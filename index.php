2751e8b685c0f39ba9cc4c81617d8b558b0867d5
pogregoire <pieroliviergregoire@gmail.com>
2016-03-04 11:00:37 +0100

    introduction de ArrayAccess dans mon_array

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <p> quelques exemple pour manipuler les iterateurs </p>
+        
         <p><a href="iterator_iterator.php">iterator_iterator</a></p>
         <p><a href="iterator_aggregate.php">iterator_aggregate</a></p>
         <p><a href="iterator.php">iterator</a></p>
